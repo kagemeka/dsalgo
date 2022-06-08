b760a81bf82e10f355d797120e843a0a47bc49dd
kagemeka <kagemeka1@gmail.com>
2022-06-08 23:25:38 +0000

    refactor ci script

--- src/rust/scripts/ci.sh
+++ src/rust/scripts/ci.sh
@@ -2,12 +2,76 @@
 
 source ~/.bashrc
 
-./scripts/update_toolchain.sh
+setup() {
+    apt update
+    apt install -y \
+        apt-utils \
+        build-essential \
+        curl
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    echo "export PATH=\"$HOME/.cargo/bin:$PATH\"" >>~/.bashrc
+}
 
-cargo update --verbose
-cargo package --list --allow-dirty
+update_toolchain() {
+    rustup toolchain install nightly
+    rustup default nightly
+    rustup component add rustfmt
+    rustup update
+    cargo update --verbose
 
-./scripts/format.sh
-./scripts/test.sh
+}
 
-cargo publish --dry-run --allow-dirty
+test() {
+    # https://doc.rust-lang.org/cargo/commands/cargo-test.html
+    cargo test
+    cargo test \
+        --all-features \
+        --all-targets \
+        --benches \
+        --bins \
+        --color always \
+        --examples \
+        --future-incompat-report \
+        --locked \
+        --manifest-path Cargo.toml \
+        --no-fail-fast \
+        --release \
+        --tests \
+        --verbose \
+        --workspace \
+        -Z unstable-options
+    # --frozen \
+    # --offline
+    # --timings html
+    # --unit-graph \
+}
+
+format() {
+    cargo fmt \
+        --all \
+        --verbose \
+        --manifest-path=Cargo.toml \
+        --message-format=human
+    # --check
+
+    ./scripts/pre-commit.sh
+}
+
+ci() {
+    if ! command -v cargo &>/dev/null; then
+        echo "command not found"
+        setup
+    fi
+    source $HOME/.cargo/env
+
+    update_toolchain
+
+    cargo package --list --allow-dirty
+
+    format
+    test
+
+    cargo publish --dry-run --allow-dirty
+}
+
+ci
--- src/rust/scripts/pre-commit.sh
+++ src/rust/scripts/pre-commit.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if ! command -v pre-commit &>/dev/null; then
-    echo "command not found"
-    ./scripts/install_pre-commit.sh
-fi
-pre-commit run --all-files
+install_pre-commit() {
+    apt update
+    apt install -y python3-pip
+    pip install -U pip
+    pip install pre-commit
+}
+
+pre-commit() {
+    if ! command -v pre-commit &>/dev/null; then
+        echo "command not found"
+        install_pre-commit
+    fi
+
+    pre-commit run --all-files
+
+}
+
+pre-commit
