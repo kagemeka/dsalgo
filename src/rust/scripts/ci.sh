de5f9b3a1c6bc516527b8f499c27af797b291989
kagemeka <kagemeka1@gmail.com>
2022-06-08 11:31:19 +0000

    update toolchain in github ci

--- src/rust/scripts/ci.sh
+++ src/rust/scripts/ci.sh
@@ -2,6 +2,7 @@
 
 source ~/.bashrc
 
+./scripts/update_toolchain.sh
 cargo update --verbose
 cargo package --list --allow-dirty
 
@@ -9,3 +10,5 @@ cargo package --list --allow-dirty
 ./scripts/test.sh
 
 cargo publish --dry-run --allow-dirty
+
+pre-commit run --all-files
