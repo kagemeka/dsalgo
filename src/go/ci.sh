27a449ee27b74ead97e94552e68a1603bc50cca0
kagemeka <kagemeka1@gmail.com>
2022-06-15 02:15:37 +0000

    reinstall go while workflow

--- src/go/ci.sh
+++ src/go/ci.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 setup() {
-
     GO_VERSION=1.18.3
     apt update
+    rm -rf /usr/local/go
     apt install -y wget
     wget -O - https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz | tar -xzC /usr/local/
-    GOPATH="/usr/local/go"
-    echo "export GOPATH=${GOPATH}" >>~/.bashrc
+    echo "export PATH=${PATH}:/usr/local/go/bin" >>~/.bashrc
     source ~/.bashrc
-    echo "export PATH=${PATH}:${GOPATH}/bin" >>~/.bashrc
-    # run source ~/.bashrc in console.
+    go version
 }
 
 help() {
@@ -29,4 +27,5 @@ ci() {
     go test -v ./...
 }
 
+$@
 ci
