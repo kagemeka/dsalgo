ba717c7750bdfc818d70145ee4fa079b00a8e2fb
kagemeka <kagemeka1@gmail.com>
2022-06-27 00:15:28 +0000

    swift conf

--- src/swift/Sources/Io.swift
+++ src/swift/Sources/Io.swift
@@ -1,25 +1,26 @@
 private func readStrings() -> [String] {
-    return readLine()!.split(separator: " ").map { String($0) }
+  return readLine()!.split(separator: " ").map { String($0) }
 }
 private class Scanner {
-    private var tokens: [String] = []
-    init() {}
-    func string() -> String {
-        while tokens.isEmpty {
-            tokens = readStrings().reversed()
-        }
-        return tokens.popLast()!
+  private var tokens: [String] = []
+  init() {}
+  func string() -> String {
+    while tokens.isEmpty {
+      tokens = readStrings().reversed()
     }
-    func strings(size: Int) -> [String] {
-        return (0..<size).map { _ in string() }
-    }
-    func int() -> Int {
-        return Int(string())!
-    }
-    func ints(size: Int) -> [Int] {
-        return (0..<size).map { _ in int() }
-    }
-    func intMatrix(height: Int, width: Int) -> [[Int]] {
-        return (0..<height).map { _ in (0..<width).map { _ in int() } }
-    }
+    return tokens.popLast()!
+  }
+  func strings(size: Int) -> [String] {
+    return (0..<size).map { _ in string() }
+  }
+  func int() -> Int {
+    return Int(string())!
+  }
+  func ints(size: Int) -> [Int] {
+    return (0..<size).map { _ in int() }
+  }
+  func intMatrix(h: Int, w: Int) -> [[Int]] {
+    return (0..<h).map { _ in (0..<w).map { _ in int() } }
+  }
+
 }
--- src/swift/ci.sh
+++ src/swift/ci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-setup() {
+install_swift() {
     SOURCEKIT_TOOLCHAIN_PATH=/usr/local/swift
-    SWIFT_VER=5.6.1
+    SWIFT_VER=5.6.2
     apt update
     DEBIAN_FRONTEND=noninteractive apt install -y \
         binutils \
@@ -24,8 +24,45 @@ setup() {
     wget -O - https://swift.org/builds/swift-${SWIFT_VER}-release/ubuntu2004/swift-${SWIFT_VER}-RELEASE/swift-${SWIFT_VER}-RELEASE-ubuntu20.04.tar.gz | tar -xzC /usr/local/ \
         --transform=s/-${SWIFT_VER}-RELEASE-ubuntu20.04//
     echo "export \"PATH=/usr/local/swift/usr/bin:$PATH\"" >>~/.bashrc
-
-    # run source ~/.bashrc in console.
+    source ~/.bashrc
 }
 
-setup
+install_swiftlint() {
+    git clone https://github.com/realm/SwiftLint.git
+    p=$(pwd)
+    cd SwiftLint
+    swift build -c release
+    mv .build/release/swiftlint /usr/local/bin/
+    cd $p
+    rm -r SwiftLint
+}
+
+install_swift-format() {
+    BRANCH=release/5.6
+    git clone -b $BRANCH https://github.com/apple/swift-format.git
+    p=$(pwd)
+    cd swift-format
+    swift build -c release
+    mv .build/release/swift-format /usr/local/bin/
+    cd $p
+    rm -r swift-format
+}
+
+setup() {
+    install_swift
+    install_swiftlint
+    install_swift-format
+}
+
+format() {
+    swift-format -p -r -i .
+    ./../../scripts/pre-commit.sh
+}
+ci() {
+    # setup
+    format
+    swiftlint
+
+}
+
+ci
