e39726a2bc2dd9e9566ea043d11b02e00430a667
kagemeka <kagemeka1@gmail.com>
2022-07-03 10:59:01 +0000

    functional_graph_kth_from

---
+++ src/cplusplus/dsalgo/functional_graph_kth_from.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+using namespace std;
+
+class functional_graph_kth_from {
+  vector<int> path;
+  int cycle_size;
+  int cycle_start_idx;
+
+public:
+  functional_graph_kth_from(const vector<int>& f, int src) {
+    int n = f.size();
+    vector<int> order(n, -1);
+
+    for (int i = src, j = 0;; j++) {
+      if (order[i] != -1) {
+        cycle_size = j - order[i];
+        cycle_start_idx = order[i];
+        break;
+      }
+      order[i] = j;
+      path.push_back(i);
+      i = f[i];
+    }
+  }
+
+  auto operator()(long k) -> const int& {
+    if (k >= cycle_start_idx)
+      k = (k - cycle_start_idx) % cycle_size + cycle_start_idx;
+    return path[k];
+  }
+};
