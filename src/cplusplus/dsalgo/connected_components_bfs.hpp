5771765f068f60b11f9a3dbe909b4ec7d6891945
kagemeka <kagemeka1@gmail.com>
2022-07-04 09:13:50 +0000

    rename label -> id

--- src/cplusplus/dsalgo/connected_components_bfs.hpp
+++ src/cplusplus/dsalgo/connected_components_bfs.hpp
@@ -7,20 +7,20 @@ template <typename G>
 auto connected_components(const G& g) -> vector<int> {
   int n = g.size();
   queue<int> que;
-  vector<int> labels(n, -1);
-  int l = 0;
+  vector<int> ids(n, -1);
+  int id = 0;
   for (int i = 0; i < n; i++) {
-    if (labels[i] != -1) continue;
+    if (ids[i] != -1) continue;
     que.push(i);
     while (!que.empty()) {
       int u = que.front();
       que.pop();
-      labels[u] = l;
+      ids[u] = id;
       for (auto& v : g[u]) {
-        if (labels[v] == -1) que.push(v);
+        if (ids[v] == -1) que.push(v);
       }
     }
-    l++;
+    id++;
   }
-  return labels;
+  return ids;
 }
--- src/cplusplus/dsalgo/connected_components_dfs_recurse.hpp
+++ src/cplusplus/dsalgo/connected_components_dfs_recurse.hpp
@@ -7,20 +7,20 @@ using namespace std;
 template <typename G>
 auto connected_components(const G& g) -> vector<int> {
   int n = g.size();
-  vector<int> labels(n, -1);
-  int l = 0;
+  vector<int> ids(n, -1);
+  int id = 0;
   function<void(int)> dfs;
   dfs = [&](int u) {
-    labels[u] = l;
+    ids[u] = id;
     for (auto& v : g[u]) {
-      if (labels[v] != -1) continue;
+      if (ids[v] != -1) continue;
       dfs(v);
     }
   };
   for (int i = 0; i < n; i++) {
-    if (labels[i] != -1) continue;
+    if (ids[i] != -1) continue;
     dfs(i);
-    l++;
+    id++;
   }
-  return labels;
+  return ids;
 }
