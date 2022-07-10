b562ab30493b525a0b413bf56602e5ca125abbad
kagemeka <kagemeka1@gmail.com>
2022-07-10 02:06:14 +0000

    number of distinct substrings

---
+++ src/rust/src/number_of_distinct_substrings.rs
@@ -0,0 +1,26 @@
+use crate::{
+    longest_common_prefix_array_kasai::lcp_array,
+    suffix_array_induced_sort_recurse::sa_is,
+};
+pub fn count_substrings(a: Vec<usize>) -> usize {
+    let sa = sa_is(a.clone());
+    let lcp = lcp_array(&a, &sa);
+    let n = sa.len();
+    n * (n + 1) / 2 - lcp.iter().sum::<usize>()
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        let cases = vec![
+            (vec![0, 1, 2, 1, 2, 1, 0], 21),
+            (vec![1, 0, 3, 3, 0, 3, 3, 0, 2, 2, 0], 53),
+            (vec![0, 1, 0, 1, 0, 2, 0, 2, 0], 33),
+            (vec![0, 0, 0, 0, 0], 5),
+        ];
+        for (a, ans) in cases {
+            assert_eq!(count_substrings(a), ans);
+        }
+    }
+}
