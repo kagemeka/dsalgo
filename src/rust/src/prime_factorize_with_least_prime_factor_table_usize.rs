564201e990863f83edc8770c3710dcf8acb18980
kagemeka <kagemeka1@gmail.com>
2022-08-01 15:28:06 +0000

    prime factorize with lpf usize

---
+++ src/rust/src/prime_factorize_with_least_prime_factor_table_usize.rs
@@ -0,0 +1,41 @@
+use crate::sieve_of_eratosthenes_least_prime_factor_table_direct::*;
+pub struct PrimeFactorize(Vec<usize>);
+impl PrimeFactorize {
+    pub fn new(size: usize) -> Self { Self(least_prime_factor(size)) }
+
+    pub fn factorize(&self, mut n: usize) -> Vec<(usize, usize)> {
+        assert!(n < self.0.len());
+        let mut factors = vec![];
+        let mut prime = 0;
+        let mut cnt = 0;
+        while n > 1 {
+            let p = self.0[n];
+            n /= p;
+            if p == prime {
+                cnt += 1;
+                continue;
+            }
+            if cnt > 0 {
+                factors.push((prime, cnt));
+            }
+            prime = p;
+            cnt = 1;
+        }
+        if cnt > 0 {
+            factors.push((prime, cnt));
+        }
+        factors
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        use crate::prime_factorize_trial_division_usize::prime_factorize;
+        let f = PrimeFactorize::new(1 << 10);
+        for i in 1..1 << 10 {
+            assert_eq!(f.factorize(i), prime_factorize(i));
+        }
+    }
+}
