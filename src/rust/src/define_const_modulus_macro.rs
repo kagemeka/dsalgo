0b4a75fa655aae411031a3ac70439cb65c8deec0
kagemeka <kagemeka1@gmail.com>
2022-08-05 07:24:04 +0000

    modular int with static modulus i64

--- src/rust/src/define_const_modulus_macro.rs
+++ src/rust/src/define_const_modulus_macro.rs
@@ -12,6 +12,7 @@ macro_rules! define_const_mod_old {
 }
 define_const_mod_old!(Mod998_244_353, u32, 998_244_353);
 define_const_mod_old!(Mod1_000_000_007, u32, 1_000_000_007);
+define_const_mod_old!(Mod1_000_000_007I64, i64, 1_000_000_007);
 #[cfg(test)]
 mod tests {
     use super::*;
