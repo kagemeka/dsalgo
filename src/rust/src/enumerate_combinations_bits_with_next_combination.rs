8a9e51991d76c2d17d95b35557386354f75b8b65
kagemeka <kagemeka1@gmail.com>
2022-08-07 18:34:45 +0000

    enumerate combinations bits with next comb

--- src/rust/src/enumerate_combinations_bits_with_next_combination.rs
+++ src/rust/src/enumerate_combinations_bits_with_next_combination.rs
@@ -1,34 +1,28 @@
 use crate::next_combination_bits::next_combination;
-// def combinations_next_comb(
-//     n: int,
-//     k: int,
-// ) -> typing.Generator[tuple[int, ...], None, None]:
-//     a = tuple(range(n))
-//     n = len(a)
-//     if k < 0 or n < k:
-//         return
-//     if k == 0:
-//         yield ()
-//         return
-//     limit = 1 << n
-//     s = (1 << k) - 1
-//     while s < limit:
-//         yield tuple(a[i] for i in range(n) if s >> i & 1)
-//         s = next_combination(s)
 pub fn combinations(n: usize, k: usize) -> Vec<usize> {
     assert!(k <= n);
     if k == 0 {
         return vec![0];
     }
+    let offset = (std::mem::size_of::<usize>() << 3) - n;
     let mut res = vec![];
-    let mut s = (1 << k) - 1;
+    let mut s: usize = (1 << k) - 1;
     let lim = 1 << n;
-    while s < lim {}
+    while s < lim {
+        res.push(s.reverse_bits() >> offset);
+        s = next_combination(s);
+    }
     res.reverse();
     res
 }
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn test() {}
+    fn test() {
+        dbg!(std::mem::size_of::<usize>());
+        for s in combinations(8, 4) {
+            println!("{:08b}", s);
+        }
+    }
 }
