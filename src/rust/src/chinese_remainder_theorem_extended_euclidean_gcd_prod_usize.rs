4b8286ada1fa9271b3a697bceb69e657cd528784
kagemeka <kagemeka1@gmail.com>
2022-08-20 07:55:14 +0000

    modular linear equation with crt extgcd

--- src/rust/src/chinese_remainder_theorem_extended_euclidean_gcd_prod_usize.rs
+++ src/rust/src/chinese_remainder_theorem_extended_euclidean_gcd_prod_usize.rs
@@ -1,5 +1,5 @@
 use crate::chinese_remainder_theorem_extended_euclidean_gcd_safe_usize::*;
-pub fn crt_reduce(mr: &[(usize, usize)]) -> Option<(usize, usize)> {
+pub fn crt_prod(mr: &[(usize, usize)]) -> Option<(usize, usize)> {
     let (mut m0, mut r0) = (1, 0);
     for &(m1, r1) in mr.iter() {
         let (lcm, ans) = safe_crt(m0, r0, m1, r1)?;
@@ -16,7 +16,7 @@ mod tests {
     fn test() {
         let cases = vec![(vec![(5, 3), (7, 4), (8, 3)], Some((280, 123)))];
         for (mr, ans) in cases {
-            assert_eq!(crt_reduce(&mr), ans);
+            assert_eq!(crt_prod(&mr), ans);
         }
     }
 }
