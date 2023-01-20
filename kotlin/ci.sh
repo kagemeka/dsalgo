bca63cb98477bb0fd3780b5a10a84e215a01b1f5
kagemeka <kagemeka1@gmail.com>
2023-01-20 08:23:43 +0000

    renew kotlin ci

--- kotlin/ci.sh
+++ kotlin/ci.sh
@@ -1,16 +1,53 @@
 #!/bin/bash
 
-setup() {
+source ~/.bashrc
+
+install_sdkman() {
+    # https://sdkman.io/install
     apt update
     apt install -y \
+        curl \
         unzip \
         zip
 
     curl -s https://get.sdkman.io | bash
     echo "source $HOME/.sdkman/bin/sdkman-init.sh" >>~/.bashrc
     source $HOME/.sdkman/bin/sdkman-init.sh
+    sdk version
+}
+
+install_java() {
+    # https://linuxhint.com/install-java-ubuntu-22-04/
+
+    apt update
+    apt install -y openjdk-18-jdk
+}
+
+install_kotlin() {
+    # https://kotlinlang.org/docs/command-line.html
+
+    if ! command -v sdk &>/dev/null; then
+        echo "command not found"
+        install_sdkman
+    fi
+
+    if ! command -v java &>/dev/null; then
+        echo "command not found"
+        install_java
+    fi
+
     sdk install kotlin
+    kotlin -version
+
     # run source ~/.bashrc in console.
 }
 
-setup
+ci() {
+    if ! command -v kotlin &>/dev/null; then
+        echo "command not found"
+        install_kotlin
+    fi
+
+}
+
+ci
